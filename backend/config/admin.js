9ae5d14c10ecf390ee0da694e3c726049a4ae0e2
JFeiss <150168366+JFeiss@users.noreply.github.com>
2024-08-21 13:33:05 -0400

    Update admin.js

--- backend/config/admin.js
+++ backend/config/admin.js
@@ -7,7 +7,7 @@ module.exports = ({ env }) => ({
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT', 'a3da8ef22ff499a66456726e97085a57'),
+      salt: env('TRANSFER_TOKEN_SALT', 'c81240a55090ce76e379d5dd00bc7b7780fe26ae78436303dc257bc29dcf45be95200a335abd5b7805584d160c822778d740257968e52a54bac55a9203674daae591239d8956100f09b930f619ae9eaecdd905ff2b05bf46bf3fa5d97f10de98c32178585301cb61f32b24ce9d1d86e2ffd1d5fc3bd3458023ed26063a339c2a'),
     },
   },
   flags: {
